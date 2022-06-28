d6f11b57e6ca9e093dab4ccb2befa66dfe8f1f65
Ernestoc14 <descreed34@hotmail.com>
2022-06-27 22:50:11 -0500

    Up 3 Opc

--- src/App.java
+++ src/App.java
@@ -6,6 +6,11 @@
 // nombre genérico, laboratorio, existencia, fechadecaducidad(MM/AAAA),
 // fechaderegistro(dia-mes-año). La aplicación debe contar con las siguientes opciones
 
+//Nombre com, gen, exit, COMUN
+//1-F Cad
+//2-Lab
+//3-Lab, F Cad
+
 // 1-Registrar Laboratorios(crear, editar y eliminar)
 // 2-Definir los Anaqueles(crear, editar y eliminar)
 // 3-Registrar medicamentos en el inventario(se debe poder editar y eliminar)
@@ -26,53 +31,60 @@
 import java.util.Scanner;
 public class App {
     //SEXO 
-    private char[] sex;
-    //Sexo setter method 
-    public void setUserSex(char[] sex){
-        this.sex = sexo;
-    }
-    //sexo getter method
-    public char[] getUserSex(){
-        return sexo;
-    }
-    //PESO
-    private double[] pes;
-    //Peso setter method 
-    public void setUserPeso(double[] pes){
-        this.pes = peso;
-    }
-    //sexo getter method
-    public double[] getUserPeso(){
-        return pes;
-    }
-    Scanner sn = new Scanner(System.in);
-    char [] sexo = new char [15]; //Para sexo
-    double [] peso = new double [15]; //Para peso
-    double TotaldePeso=0;
+    // private char[] sex;
+    // //Sexo setter method  
+    // public void setUserSex(char[] sex){
+    //     this.sex = sexo;
+    // }
+    // //sexo getter method
+    // public char[] getUserSex(){
+    //     return sexo;
+    // }
+    // //PESO
+    // private double[] pes;
+    // //Peso setter method 
+    // public void setUserPeso(double[] pes){
+    //     this.pes = peso;
+    // }
+    // //sexo getter method
+    // public double[] getUserPeso(){
+    //     return pes;
+    // }
 
+
+    Scanner sn = new Scanner(System.in);
+    
+    String[] medicamento = new String[5]; //Para el medicamento
+    String []laboratorio = new String[5]; //Para el laboratorio
+
+    //GENERALES
+    String [][] AnaquelAF = new String [2][5]; //Anaquel de A-F
+    String [][] AnaquelGP = new String [2][5]; //Anaquel de G-P
+    String [][] AnaquelQZ = new String [2][5]; //Anaquel de Q-Z     
+    String [][] MedInfo = new String [10][7]; //Almacenamos la Info del Med como nombres, lab, fechas, Codigo...
     public void MenuOpc(int opc)
     {   App pr = new App();
         switch (opc) {
-            case 1: //Ingresar Sexo
-                pr.IngresarSexo(sexo);
+            case 1: //Registrar Laboratorios
+                pr.RegistrarLab(laboratorio, MedInfo);
                 break;
-            case 2: //Ingresar Peso
-                pr.IngresarPeso(peso);
+            case 2: //Definir los Anaqueles
+                pr.DefAnaq(AnaquelAF, AnaquelGP, AnaquelQZ, MedInfo);
                 break;
-            case 3: //Mostrar Promedio
-                pr.MostrarPromedio(TotaldePeso,peso);
+            case 3: //Registrar medicamentos en el inventario
+                pr.RegistrarMedEnInventario(AnaquelAF, AnaquelGP, AnaquelQZ, MedInfo, medicamento);
                 break;
             case 4: //El Peso de la persona mas pesada
-                pr.PersonaMasPesada(peso);
+                
                 break;
             case 5: //Numero de personas con contextura delgada por sexo 
-                pr.DelgadaXSexo(peso);
+                
                 break;
             case 6: //Numero de personas con contextura mediana por sexo 
-                pr.MedianaXSexo(peso);
+                
                 break;
             case 7: //Numero de personas con contextura gruesa por sexo 
-                pr.GruesaXSexo(peso);
+                
                 break;
             case 8: //Salir
                 break;
@@ -80,95 +92,109 @@ public class App {
                 break;
         }
     }
-    public void IngresarSexo(char []sexo)  //OPCION 1
-    {   App pr = new App();
+    public void RegistrarLab(String []laboratorio, String[][] MedInfo)  //OPCION 1
+    {   //App pr = new App();
         int opc=0,wi=0;
-        char verif=' ';
-        System.out.println("Desea ingresar el sexo de un usuario? 1-Si 2-No: ");
+        System.out.println("Desea registrar un laboratorio? 1-Si 2-No: ");
         opc = sn.nextInt();
+        sn.nextLine();
         while(opc==1)
-        {   System.out.printf("Deme el sexo de la persona %d :",wi+1);
-            verif = sn.next().charAt(0);
-            if((verif=='f')||(verif=='m')){
-                sexo[wi] = verif;
+        {   System.out.printf("Deme el nombre del laboratorio %d: ",wi+1);
+            laboratorio[wi] = sn.nextLine();
+            MedInfo[wi][3] = laboratorio[wi];
+            wi++;
+            System.out.println("Desea registrar otro laboratorio? 1-Si 2-No: ");
+            opc = sn.nextInt();
+            sn.nextLine();
+        }
+        //pr.setUserSex(sex);
+    }
+    public void DefAnaq(String [][] AnaquelAF, String [][] AnaquelGP, String [][] AnaquelQZ, String [][] MedInfo) //OPCION 2
+    {   //App pr = new App();
+        int opc=0,wi=0;
+        System.out.println("Desea definir un anaquel? 1-Si 2-No: ");
+        opc = sn.nextInt();
+        sn.nextLine();
+        while(opc==1)
+        {   System.out.printf("Deme el peso de la persona %d :", wi+1);
+            wi++;
+            System.out.print("Desea definir otro anaquel? 1-Si 2-No: ");
+            opc = sn.nextInt();
+            sn.nextLine();
+        }
+        //pr.setUserPeso(peso);
+    }
+    public void RegistrarMedEnInventario(String [][] AnaquelAF, String [][] AnaquelGP, String [][] AnaquelQZ, String [][] MedInfo, String [] medicamento) //OPCION 3 
+    {    //App pr = new App();
+        int opc=0,wi=0;
+        String lab;
+        System.out.println("Desea registrar un Medicamento en el Inventario? 1-Si 2-No: ");
+        opc = sn.nextInt();
+        sn.nextLine();
+        while(opc==1)
+        {   System.out.printf("Deme el nombre comercial del medicamento :");
+            medicamento[wi] = sn.nextLine();
+            MedInfo[wi][0] = medicamento[wi]; //Nomb Comercial  0
+            System.out.printf("Deme el nombre generico del medicamento :");
+            MedInfo[wi][1] = sn.nextLine();  //Nomb Generico   1
+            System.out.printf("Deme la existencia del medicamento %s:",MedInfo[wi][0]);
+            MedInfo[wi][2] = sn.nextLine();  //Existencia  2
+           
+            System.out.printf("Deme la fecha de caducidad del medicamento %s en formato MM/AAAA :",MedInfo[wi][0]);
+            MedInfo[wi][4] = sn.nextLine(); //Fecha de Caducidad  4
+            System.out.printf("Deme la fecha de registro del medicamento %s en formato D/MM/AA :",MedInfo[wi][0]);
+            MedInfo[wi][5] = sn.nextLine(); //Fecha de Registro  5
+            System.out.printf("Deme el codigo del medicamento %s: ",MedInfo[wi][0]);
+            MedInfo[wi][6] = sn.nextLine();  //Codigo 6
+
+            lab = MedInfo[wi][3];  //Laboratorio 3
+            char firstletter = lab.charAt(0);
+
+            if( Character.toString(firstletter).matches("[A-F?]") )  //Anaquel 1
+            {   AnaquelAF [0][wi] = MedInfo[wi][3];  // Laboratorio
+                AnaquelAF [1][wi] = MedInfo[wi][0]; //Nombre Comercial
+                System.out.printf("El medicamento %s se agrego al Anaquel 1", MedInfo[wi][0]);
+            }
+            else
+            {   if( Character.toString(firstletter).matches("[G-P?]") )  //Anaquel 2
+                {   AnaquelGP [0][wi] = MedInfo[wi][3];  // Laboratorio
+                    AnaquelGP [1][wi] = MedInfo[wi][0]; //Nombre Comercial
+                    System.out.printf("El medicamento %s se agrego al Anaquel 2", MedInfo[wi][0]);
+                }
+                else
+                {   if( Character.toString(firstletter).matches("[Q-Z?]") )  //Anaquel 3
+                    {   AnaquelQZ [0][wi] = MedInfo[wi][3];  // Laboratorio
+                        AnaquelQZ [1][wi] = MedInfo[wi][0]; //Nombre Comercial
+                        System.out.printf("El medicamento %s se agrego al Anaquel 3", MedInfo[wi][0]);
+                    }
+                    else
+                    {   System.out.println("Error");
+                    }
+                }
             }
             wi++;
-            System.out.print("Desea ingresar el sexo de un usuario? 1-Si 2-No: ");
+            System.out.print("\nDesea registrar otro Medicamento en el Inventario? 1-Si 2-No: ");
             opc = sn.nextInt();
+            sn.nextLine();
         }
-        pr.setUserSex(sex);
+        //pr.setUserSex(sex);
     }
-    public void IngresarPeso(double []peso) //OPCION 2
-    {   App pr = new App();
-        int opc=0,wi=0;
-        System.out.println("Desea ingresar el peso de un usuario? 1-Si 2-No: ");
-        opc = sn.nextInt();
-        while(opc==1)
-        {   System.out.printf("Deme el peso de la persona %d :",wi+1);
-            peso[wi]=sn.nextDouble();
-            wi++;
-            System.out.print("Desea ingresar el sexo de un usuario? 1-Si 2-No: ");
-            opc = sn.nextInt();
-        }
-        pr.setUserPeso(peso);
-    }
-    public void MostrarPromedio(double TotaldePeso, double []peso) //OPCION 3 
-    {   double PesoProm=0;
-        for(int fi=0;fi<peso.length;fi++)
-        {   TotaldePeso = TotaldePeso + peso[fi];
-        }
-        PesoProm = TotaldePeso/peso.length;
-        System.out.printf("El peso promedio es de %.2f ", PesoProm);
-    }
-    public void PersonaMasPesada(double []peso)  //OPCION 4
-    {   double mayor = peso[0];
-        for(int fi=0;fi<peso.length;fi++)
-        {   if(peso[fi] > mayor){
-            mayor = peso[fi];
-            }
-        }
-        System.out.printf("La persona mas pesada peso %.2f ",mayor);
-    }
-    public void DelgadaXSexo(double []peso) //OPCION 5
-    {   int delgadas=0;
-        for(int fi=0;fi<peso.length;fi++)
-        {   if(peso[fi] < 53 ){
-            delgadas++;
-            }
-        }
-        System.out.printf("Hay %d personas delgadas en el gimnasio ",delgadas);
-    }
-    public void MedianaXSexo(double []peso) //OPCION 6
-    {   int medianas=0;
-        for(int fi=0;fi<peso.length;fi++)
-        {   if((peso[fi] >= 53) && (peso[fi]<=60)){
-            medianas++;
-            }
-        }
-        System.out.printf("Hay %d personas medianas en el gimnasio ",medianas);
-    }
-    public void GruesaXSexo(double []peso) //OPCION 7
-    {   int gruesas=0;
-        for(int fi=0;fi<peso.length;fi++)
-        {   if(peso[fi] > 60){
-            gruesas++;
-            }
-        }
-        System.out.printf("Hay %d personas medianas en el gimnasio ",gruesas);
-    }
+    
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
         App pr = new App();
         int y=0,opc=0;
-        do{ System.out.println("--------------------------------Menu-----------------------------------------------");
-            System.out.println("1.Ingresar Sexo ");
-            System.out.println("2.Ingresar Peso ");
-            System.out.println("3.Mostrar Promedio ");
-            System.out.println("4.El Peso de la persona mas pesada ");
-            System.out.println("5.Numero de personas con contextura delgada por sexo ");
-            System.out.println("6.Numero de personas con contextura mediana por sexo ");
-            System.out.println("7.Numero de personas con contextura gruesa por sexo ");
-            System.out.println("8.Salir ");
+        do{ System.out.println("--------------------------------Menu Principal---------------------------------");
+            System.out.println("1.Registrar Laboratorios ");
+            System.out.println("2.Definir los Anaqueles ");
+            System.out.println("3.Registrar medicamentos en el inventario ");
+            System.out.println("4.Consultar medicamentos de un Laboratorio X ");
+            System.out.println("5.Consultar los laboratorios que proveen un medicamento X utilizando el nombre genérico ");
+            System.out.println("6.Consultar medicamentos cuya cantidad está por debajo de una cantidad X ");
+            System.out.println("7.Consultar medicamentos que vencen en un mes X y año X ");
+            System.out.println("8.Consultar los medicamentos que vencen en los próximos N meses ");
+            System.out.println("9.Consultar los medicamentos almacenados en un anaquel X ");
+            System.out.println("10.Salir ");
             System.out.print("Introduzca la opcion a realizar:");
             opc = sn.nextInt();
             pr.MenuOpc(opc);
@@ -180,27 +206,27 @@ public class App {
 
 
 //Sexo class con atributo sex
-    public class Sexo{
-        private char sex;
-    //Setter del valor de sex
-        public Sexo(char sex){
-            this.sex = sex;
-        }
-    //getter del valor de sex
-        public char getSex(){
-            return sex;
-        }
-    }   
-//Peso class con atributo pes
-    public class Peso{
-        private double pes;
-    //Setter del valor de pes
-        public Peso(double pes){
-        this.pes = pes;
-        }
-    //getter del valor de sex
-        public double getPes(){
-        return pes;
-        }
-    } 
+//     public class Sexo{
+//         private char sex;
+//     //Setter del valor de sex
+//         public Sexo(char sex){
+//             this.sex = sex;
+//         }
+//     //getter del valor de sex
+//         public char getSex(){
+//             return sex;
+//         }
+//     }   
+// //Peso class con atributo pes
+//     public class Peso{
+//         private double pes;
+//     //Setter del valor de pes
+//         public Peso(double pes){
+//         this.pes = pes;
+//         }
+//     //getter del valor de sex
+//         public double getPes(){
+//         return pes;
+//         }
+//     } 
 }
